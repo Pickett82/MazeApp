efc6b9308ef8b91f42835d0a7c3431897ef41ee7
James Pickett <ma0jdp@gmail.com>
2016-09-15 08:48:42 +0100

    init

---
+++ MazeApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\u2554\u2550\u2566\u2550\u2557");
+            Console.WriteLine("\u2551\u00B7\u2551\u00B7\u2551");
+            Console.WriteLine("\u2560 \u256C\u2550\u2563");
+            Console.WriteLine("\u2551\u00B7 \u00B7\u2551");
+            Console.WriteLine("\u255A\u2550\u2569\u2550\u255D");
+
+        }
+    }
+}
